d5588f04ba455258d44af201746aa872a2a044a8
Pádua Alves <paduaalves03@gmail.com>
2021-12-04 21:06:21 -0300

    Commit inicial

---
+++ src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -0,0 +1,15 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+		//fail("Not yet implemented");
+	}
+
+}
